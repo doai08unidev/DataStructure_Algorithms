01a73c8fa81e1981bdc3e76b968c0808d25a10f5
doai08unidev <doai08.unidev@gmail.com>
2022-05-24 14:56:12 +0700

    update3

--- SearchAlgorithm/SearchAlgorithmProgram.cs
+++ SearchAlgorithm/SearchAlgorithmProgram.cs
@@ -4,24 +4,103 @@ namespace SearchAlgorithm
 {
     class SearchAlgorithmProgram
     {
-        public void LinearSearch(int[] Array, int x)
+        public int LinearSearch(int[] Array, int x)
         {
             int n = Array.Length;
             for(int i=0; i<n; i++)
             {
                 if(Array[i] == x)
                 {
-                    System.Console.WriteLine("Tim duoc " + x + " tai vi tri " + (i + 1));
+                    return i;
                 }
             }
+            return -1;
             
+        }
+        public int BinarySearchRecursion(int[] Array, int x, int left, int right)
+        {
+            int mid = (left + right) / 2;
+            if (left <= right)
+            {
+                if (x == Array[mid])
+                {
+                    return mid;
+                }
+                if (x > Array[mid])
+                {
+                    return BinarySearchRecursion(Array, x, mid + 1, right);
+                }
+                if (x < Array[mid])
+                {
+                    return BinarySearchRecursion(Array, x, left, mid - 1);
+                }
+            }
+            return -1;
+        }
+        public int BinarySearchIterator(int[] Array, int x, int left, int right)
+        {
+            
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                if (x==Array[mid])
+                {
+                    return mid;
+                }
+                else if (x>Array[mid])
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+                
+            };
+            return -1;
+
         }
         static void Main(string[] args)
         {
             SearchAlgorithmProgram searchProgram = new SearchAlgorithmProgram();
-            int[] Array = { 1, 3, 5, 6, 7, 9 };
-            int x = 7;
-            searchProgram.LinearSearch(Array,x);
+            int[] Array = { 1, 3, 5, 6, 7, 9 }; //array
+            int x = 7; //number need to find
+
+            //Linear Search
+            Console.WriteLine("===LinearSearch===");
+            int linearSearchResult = searchProgram.LinearSearch(Array,x);
+            if (linearSearchResult == -1)
+            {
+                Console.WriteLine("khong tim thay");
+            }
+            else
+            {
+                Console.WriteLine("Da tim thay " + x + " o vi tri " + (linearSearchResult +1));
+            }
+
+            //BinarySearch (Recursion)
+            Console.WriteLine("===BinarySearch(Recursion)===");
+            int binarySearchRecursion = searchProgram.BinarySearchRecursion(Array, x, 0, Array.Length - 1);
+            if(binarySearchRecursion == -1)
+            {
+                Console.WriteLine("Khong tim thay");
+            }
+            else
+            {
+                Console.WriteLine("Da tim thay " + x + " o vi tri " + (binarySearchRecursion + 1));
+            }
+
+            //BinarySearch (Iterator)
+            Console.WriteLine("===BinarySearch(Iterator)===");
+            int binarySearchIterator = searchProgram.BinarySearchIterator(Array, x, 0, Array.Length - 1);
+            if (binarySearchIterator == -1)
+            {
+                Console.WriteLine("Khong tim thay");
+            }
+            else
+            {
+                Console.WriteLine("Da tim thay " + x + " o vi tri " + (binarySearchIterator + 1));
+            }
         }
 
     }
