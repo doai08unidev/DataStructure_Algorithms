1d34bd63bc569c511b19d0ff41ab8355e78d7b5c
doai08unidev <doai08.unidev@gmail.com>
2022-05-24 11:21:18 +0700

    update2

--- SearchAlgorithm/SearchAlgorithmProgram.cs
+++ SearchAlgorithm/SearchAlgorithmProgram.cs
@@ -4,24 +4,24 @@ namespace SearchAlgorithm
 {
     class SearchAlgorithmProgram
     {
-        public int LinearSearch(int[] Array, int x)
+        public void LinearSearch(int[] Array, int x)
         {
             int n = Array.Length;
             for(int i=0; i<n; i++)
             {
                 if(Array[i] == x)
                 {
-                    return i;
+                    System.Console.WriteLine("Tim duoc " + x + " tai vi tri " + (i + 1));
                 }
             }
-            return -1;
+            
         }
         static void Main(string[] args)
         {
             SearchAlgorithmProgram searchProgram = new SearchAlgorithmProgram();
             int[] Array = { 1, 3, 5, 6, 7, 9 };
             int x = 7;
-            Console.WriteLine("Tim thay"searchProgram.LinearSearch(Array,x);
+            searchProgram.LinearSearch(Array,x);
         }
 
     }
