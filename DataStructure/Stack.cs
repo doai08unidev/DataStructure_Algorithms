2b98616a7d377d376f3d8332f309ec94e5a2684a
doaidev08 <89960196+doaidev08@users.noreply.github.com>
2022-06-02 17:46:00 +0700

    Update Stack.cs

--- DataStructure/Stack.cs
+++ DataStructure/Stack.cs
@@ -1,10 +1,42 @@
  using System;
 namespace DataStructure{
-    public class Stack{
-    
+    class Stack{
+        private int[] ele;
+        private int top;
+        private int max;
+        public Stack(int size){
+            ele = new int[size];
+            top = -1;
+            max = size;
+        }
+        public void Push(int item){
+            if(top == max - 1){
+                System.Console.WriteLine("Stack overflow");
+                return;
+              
+            }else{
+                ele[++top] = item;
+            }
+        }
+        public void printStack(){
+            if(top==-1){
+                System.Console.WriteLine("Stack is Empty");
+            }else{
+                for(int i =0;i<=top;i++){
+                    System.Console.WriteLine("{0} pushed into stack", ele[i]);
+                }
+            }
+        }
         public static void Main(string[] args){
-            Console.WriteLine("hello");
-            
+            Stack stack = new Stack(5);
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+            stack.Push(40);
+            stack.Push(50);
+            stack.Push(60);
+            stack.Push(70);
+            stack.printStack();
 
         }
     }
